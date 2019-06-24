4effd71ff936f72bed177a0d604a714cd96a9d51
fubo130 <bo_fu2@baylor.edu>
2019-06-24 18:33:10 +0800

    Cart layout&load info

--- pages/Cart/Cart.js
+++ pages/Cart/Cart.js
@@ -5,14 +5,86 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        CartItem: [],
+        CartItemID: [],
+        Selected: [],
+        SubTotal: 0,
+        IsEmpty: true,
+        /**
+         * 这项是要modif的
+         */
+        Price: [],
+        Amount: [],
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        var that = this;
+        let uInfo = wx.getStorageSync("userInfo");
+        let data = JSON.parse(uInfo);
+        wx.showLoading({
+            title: '请稍后......',
+        })
 
+        wx.request({
+            url: 'https://hb9.api.yesapi.cn/?s=App.Table.FreeFindOne&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&where=[["BindOpenID","=","' + data.BindOpenID + '"]]&fields=Cart',
+            success(res) {
+                console.log(res)
+                that.setData({
+                    CartItemID: res.data.data.data.Cart.split(",")
+                })
+                 console.log(that.data.CartItemID);
+                if (that.data.CartItemID[0] == -1) {
+                    that.setData({
+                        SubTotal: 0,
+                        IsEmpty: true
+                    })
+                }
+                else {
+                    that.setData({
+                        SubTotal: 0,
+                        IsEmpty: false
+                    })
+                    // console.log(that.data);
+                    var lst = [];
+                    var tp =[];
+                    var a = [];
+                    for (var i = 0; i < that.data.CartItemID.length; i++) {
+                        wx.request({
+                            url: 'https://hb9.api.yesapi.cn/?s=App.Table.FreeFindOne&model_name=Store_Item&app_key=74928B74E87AC199A83A17EEDB749F0A&where=[["id", "=", "' + that.data.CartItemID[i] + '"]]&return_sql=true&page=1&perpage=100',
+                            success(res) {
+                                console.log(res);
+                                lst.push(res.data.data.data);
+                                var x = res.data.data.data.Price.split(",");
+                                a.push(1);
+                                tp.push(x[0]);
+                                console.log("x",x);
+                                console.log(tp);
+                            },
+                            fail() { },
+                            complete() {
+                                that.setData({
+                                    CartItem: lst,
+                                    Price: tp,
+                                    Amount: a
+                                })
+                                console.log("CartItem: " + that.data.CartItem[0].Item_Img)
+                                console.log("Price: " + that.data.Price)
+                                console.log("Amount: " + that.data.Amount)
+                            }
+                        })
+                        
+                    }
+                    
+                }
+            },
+            fail() {},
+            complete() {
+                wx.hideLoading();
+            }
+        })
     },
 
     /**
@@ -62,5 +134,25 @@ Page({
      */
     onShareAppMessage: function () {
 
+    },
+
+    dec:function(res) {
+    //     // console.log(res.currentTarget.id);
+    //     var tmp;
+    //     var ls;
+    //     tmp = this.data.Amount[res.currentTarget.id];
+    //     console.log(tmp);
+    //     // if (tmp>0) {
+    //         tmp = tmp-1;
+    //         for (var i = 0; i < this.data.Amount.length; i++) {
+    //             ls[i] = this.data.Amount[i]
+    //         // }
+    //         ls[res.currentTarget.id] = tmp;
+    //         console.log("ls: "+ls);
+    //         this.setData({
+    //             Amount: ls
+    //         })
+    //     }
+    //     console.log(this.data.Amount);
     }
 })
--- pages/ItemDetail/ItemDetail.js
+++ pages/ItemDetail/ItemDetail.js
@@ -17,18 +17,44 @@ Page({
     },
 
     onLoad: function (options) {
+        var that = this;
+        let uInfo = wx.getStorageSync("userInfo");
+        let data = JSON.parse(uInfo);
         wx.showLoading({
             title: '请稍后......',
         })
+
+        wx.request({
+            url: 'https://hb9.api.yesapi.cn/?s=App.Table.FreeFindOne&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&where=[["BindOpenID","=","' + data.BindOpenID + '"]]&fields=User_Appointment',
+            success(res) {
+                console.log(res);
+                var tmp = res.data.data.data.User_Appointment.split(',');
+                console.log(tmp);
+                if (tmp.indexOf(that.data.ItemID)==-1) {
+                    that.setData({
+                        like_btn: "../../images/like1.png"
+                    })
+                }
+
+                else {
+                    that.setData({
+                        like_btn: "../../images/like0.png"
+                    })
+                }
+            },
+            fail() {},
+            complete(){}
+        })
+
+
         console.log(options);
-        var that = this;
+        
         that.setData({
             ItemID: options.ItemID
         })
         wx.request({
             url: 'https://hb9.api.yesapi.cn/?s=App.Table.FreeQuery&model_name=Store_Item&app_key=74928B74E87AC199A83A17EEDB749F0A&where=[["id", "=", "+'+options.ItemID+'"]]&return_sql=true',
             success:function(res) {
-                console.log(res);
                 that.setData({
                     ItemPrice: res.data.data.list[0].Price,
                     ItemName: res.data.data.list[0].Item_Name,
@@ -46,7 +72,7 @@ Page({
                  that.setData({
                      DetailImg: tmp
                  })
-                console.log(that.data.ItemName + "\nPrice: " + that.data.ItemPrice + "\nCategory: " + that.data.ItemCategory)
+                // console.log(that.data.ItemName + "\nPrice: " + that.data.ItemPrice + "\nCategory: " + that.data.ItemCategory)
             },
             complete:function() {
                 wx.hideLoading();
@@ -72,119 +98,80 @@ Page({
     switchLike: function() {
         var that = this;
         //收藏
-        if (that.data.like_btn == "../../images/like1.png") {
-            wx.showToast({
-                title: '您已成功收藏',
-                duration: 1500,
-                image: '../../images/like.png'
-            })
-            /*
-            wx.showLoading({
-                title: '请稍后......',
-            })
-            let uInfo = wx.getStorageSync("userInfo");
-            let data = JSON.parse(uInfo);
-            wx.request({
-                url: 'https://hb9.api.yesapi.cn/?s=App.Table.FreeFindOne&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&where=[["BindOpenID","=","' + data.BindOpenID + '"]]&fields=User_Appointment',
-                success: function (e) {
-                    console.log(e);
-                    var appointment = [];
-                    if (e.data.data.data.User_Appointment != "") {
-                        console.log("有")
-                        appointment += ",";
-                        appointment += that.data.ItemID;
-                    }   
-                    else {
-                        console.log("空")
-                        appointment += that.data.ItemID;
+        let uInfo = wx.getStorageSync("userInfo");
+        let data = JSON.parse(uInfo);
+        // console.log(data.BindOpenID)
+        wx.request({
+            url: 'https://hb9.api.yesapi.cn/?s=App.Table.FreeFindOne&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&where=[["BindOpenID","=","' + data.BindOpenID + '"]]&fields=User_Appointment',
+            success(res) {
+                console.log(res);
+                var tmp = res.data.data.data.User_Appointment.split(',');
+                //delete tmp[tmp.indexOf(undefined)];
+                
+                console.log(tmp);
+                if (tmp[0] == ",") {
+                    tmp.splice(0,1,",");
+                }
+                if (tmp.indexOf(that.data.ItemID) == -1) {
+                    tmp.push(that.data.ItemID);
+                    console.log("不存在（添加）："+tmp)
+                }
+                else {
+                    tmp.splice(tmp.indexOf(that.data.ItemID), 1, that.data.ItemID);
+                    delete tmp[tmp.indexOf(that.data.ItemID)];
+                    console.log("存在（删除）：" + tmp)
+                }
+                for (var i = 0; i < tmp.length; i++) {
+                    if (tmp[i] == "" || tmp[i] == undefined) {
+                        tmp.splice(i, 1);
                     }
-                    console.log(appointment)
-                    var uID = e.data.data.data.id 
-                    wx.request({
-                        url: 'https://hb9.api.yesapi.cn/?s=App.Table.Update&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&id=' + uID + '&data={"User_Appointment":"' + appointment + '"}',
-                        success: function () {
-                            console.log("收藏更新")
-                        },
-                        complete:function() {
+                }
+                wx.request({
+                    url: 'https://hb9.api.yesapi.cn/?s=App.Table.Update&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&id=' + res.data.data.data.id + '&data={"User_Appointment":"' + tmp + '"}',
+                    success(res) {
+                        console.log(res);
+                        if (that.data.like_btn == "../../images/like1.png") {
                             that.setData({
                                 like_btn: "../../images/like0.png"
                             })
-                            wx.hideLoading();
                         }
-                    })
-                }
-            })*/
-            that.setData({
-                like_btn: "../../images/like0.png"
-            })
-        }
 
-        //取消
-        else {
-            wx.showToast({
-                title: '您已取消收藏',
-                duration: 1500,
-                image: '../../images/disLike.png'
-            })
-            /*
-            wx.showLoading({
-                title: '请稍后......',
-            })
-            let uInfo = wx.getStorageSync("userInfo");
-            let data = JSON.parse(uInfo);
-            wx.request({
-                url: 'https://hb9.api.yesapi.cn/?s=App.Table.FreeFindOne&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&where=[["BindOpenID","=","' + data.BindOpenID + '"]]&fields=User_Appointment',
-                success: function (e) {
-                    console.log(e);
-                    var appointment = [];
-                    var i = 0;
-                    appointment = e.data.data.data.User_Appointment.split(",");
-                    console.log(appointment)
-                    for (i = 0; i < appointment.length; i++) {
-                        if (appointment[i] = ItemID) {
-                            console.log("find");
-                        }
-                    }
-                    console.log(appointment)
-                    var uID = e.data.data.data.id
-                    wx.request({
-                        url: 'https://hb9.api.yesapi.cn/?s=App.Table.Update&model_name=Store_Users&app_key=74928B74E87AC199A83A17EEDB749F0A&id=' + uID + '&data={"User_Appointment":"' + appointment + '"}',
-                        success: function () {
-                            console.log("收藏更新")
-                        },
-                        complete: function () {
+                        else {
                             that.setData({
-                                like_btn: "../../images/like0.png"
+                                like_btn: "../../images/like1.png"
                             })
-                            wx.hideLoading();
                         }
-                    })
-                }
-            })*/
-            that.setData({
-                like_btn: "../../images/like1.png"
-            })
-        }
-    },
+                    },
+                    fail() { },
+                    complete() {
 
-    desSelt1: function () {
-        this.setData({
-            DesSelect: 1
+                    }
+                })
+            },
+            fail() { },
+            complete() {
+
+            }
         })
     },
+     desSelt1: function () {
+         this.setData({
+             DesSelect: 1
+         })
+     },
 
-    desSelt2: function() {
-        this.setData({
-            DesSelect: 2
-        })
-    },
+     desSelt2: function() {
+         this.setData({
+             DesSelect: 2
+         })
+     },
     previewImg: function(e) {
-        var index = e.currentTarget.dataset.id;
-        console.log(e)
-        wx.previewImage({
-            current: this.data.DetailImg[index],     //当前图片地址
-            urls: this.data.DetailImg,               //所有要预览的图片的地址集合 数组形式
-            success: function (e) { },
+         var index = e.currentTarget.dataset.id;
+         console.log(e)
+         wx.previewImage({
+             current: this.data.DetailImg[index],     //当前图片地址
+             urls: this.data.DetailImg,               //所有要预览的图片的地址集合 数组形式
+             success: function (e) { },
             fail: function (e) { },
             complete: function (e) { },
         })
